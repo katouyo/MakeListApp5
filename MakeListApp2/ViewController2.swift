ccc13305e7e2e5199bbb9f08b5b100715802a721
katouyo <yothiac13@gmail.com>
2018-10-26 11:23:48 +0900

    first commit

---
+++ MakeListApp2/ViewController2.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2.swift
+//  MakeListApp2
+//
+//  Created by Yoshiaki Kato on 2018/10/25.
+//  Copyright © 2018 Yoshiaki Kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# MakeListApp5
